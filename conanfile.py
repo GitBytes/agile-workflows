50e81fa97c81cf879f94411ea20c456883478cca
Marco Minutoli <marco.minutoli@pnnl.gov>
2022-01-18 14:03:09 -0800

    Add build files and update build instructions.

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+
+
+class AgileWorkflowsConan(ConanFile):
+    name = "agile-workflows"
+    version = "0.0.1"
+    license = "APACHE 2.0"
+    author = "<Put your name here> <And your email here>"
+    # url = "<Package recipe repository url here, for issues about the package>"
+    description = "The AGILE Workflows"
+    # topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    # options = {"shared": [True, False], "fPIC": [True, False]}
+    # default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires('gmt/2.0.0@user/stable')
+        self.requires('shad/1.0.0@user/stable')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        pass
