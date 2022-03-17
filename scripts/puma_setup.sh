500f4a589bfebd81343b041f5345b675a8c63eaa
Marco Minutoli <marco.minutoli@pnnl.gov>
2022-03-17 14:42:51 -0700

    Add support for riscv build.

--- scripts/puma_setup.sh
+++ scripts/puma_setup.sh
@@ -7,5 +7,12 @@ pip install --user conan
 conan profile new default --detect &> /dev/null
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
+if grep riscv $HOME/.conan/settings.yml; then
+    echo RISCV support already added. Skipping.
+else
+    echo RISCV support added.
+    sed -i .bkp s/x86/x86, riscv/ $HOME/.conan/settings.yml
+fi
+
 conan create conan/gmt user/stable
 conan create conan/shad user/stable
