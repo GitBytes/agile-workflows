1507b6099348389ced70a4847087cc5d0ab6cfba
Marco Minutoli <marco.minutoli@pnnl.gov>
2022-03-17 17:28:01 -0700

    Add setup scripts for cori and riscv platform

---
+++ scripts/cori_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Running the first time setup script'
+
+pip install --user conan
+
+conan profile new default --detect &> /dev/null
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan profile update env.CC=$(which gcc) default
+conan profile update env.CXX=$(which g++) default
+
+if [ -d $HOME/.conan/data ]; then
+    rm -rf $HOME/.conan/data
+fi
+
+if grep riscv $HOME/.conan/settings.yml; then
+    echo RISCV support already added. Skipping.
+else
+    echo RISCV support added.
+    sed -i .bkp s/x86/x86, riscv/ $HOME/.conan/settings.yml
+fi
+
+conan create conan/gmt user/stable
+conan create conan/shad user/stable
