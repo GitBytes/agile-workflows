c45df7565c9a3568af0ba7257abbad04ad7a46cb
John Feo <john.feo@pnnl.gov>
2022-10-05 22:57:02 -0700

    copywrite and disclaimer header

--- wk2_approx/approxMatching.cc
+++ wk2_approx/approxMatching.cc
@@ -1,27 +1,35 @@
-//// person of interest = 1128501731262832684
-//===------------------------------------------------------------*- C++ -*-===//
-//
-//                                     SHAD
-//
-//      The Scalable High-performance Algorithms and Data Structure Library
-//
-//===----------------------------------------------------------------------===//
-//
-// Copyright 2018 Battelle Memorial Institute
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy
-// of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-//
-//===----------------------------------------------------------------------===/
+***************************************** Pre-copyright Notice *****************************************
+
+This computer software was prepared by Battelle Memorial Institute, hereinafter the Contractor, under
+Contract No. DE-AC05-76RL01830 with the Department of Energy (DOE).  All rights in the computer soft-
+ware are reserved by DOE on behalf of the United States Government  and the Contractor as provided in
+the Contract. You are authorized to use this computer software for Governmental purposes  but  it  is
+not to be released or distributed to the public.  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY
+WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  This notice in-
+cluding this sentence must appear on any copies of this computer software.
+
+********************************************** Disclaimer **********************************************
+
+This material was prepared as an account of work sponsored by an agency of the United States Government.
+Neither the United States Government nor the United States Department of Energy, nor Battelle,  nor  any
+of their employees, nor any jurisdiction or organization that has cooperated in the development of these
+materials, makes any warranty, express or implied,  or assumes any legal liability or responsibility for
+the accuracy, completeness, or usefulness or any information, apparatus, product, software,  or  process
+disclosed, or represents that its use would not infringe privately owned rights. Reference herein to any
+specific commercial product, process, or service by trade name, trademark,  manufacturer,  or  otherwise
+does not necessarily constitute or imply its endorsement, recommendation,  or  favoring  by  the  United
+States Government or any agency thereof,  or  Battelle  Memorial  Institute.   The views and opinions of
+authors expressed herein do not necessarily state or reflect those of the  United  States  Government or
+any agency thereof.
+
+PACIFIC NORTHWEST NATIONAL LABORATORY
+            operated by
+             BATTELLE
+             for the
+ UNITED STATES DEPARTMENT OF ENERGY
+ under Contract DE-AC05-76RL01830
+
+********************************************************************************************************
 
 #include <mutex>
 #include <algorithm>
