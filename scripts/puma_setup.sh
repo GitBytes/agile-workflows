9d2c88ac97ffd7c70efb97f31e4924ac99d80bc5
Marco Minutoli <marco.minutoli@pnnl.gov>
2023-02-24 11:40:51 -0800

    Avoid the installation of conan2.

--- scripts/puma_setup.sh
+++ scripts/puma_setup.sh
@@ -2,7 +2,7 @@
 
 echo 'Running the first time setup script'
 
-pip install --user conan --upgrade
+pip install --user "conan==1.59"
 pip install --user torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
 pip install --user torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cpu.html
 if [ ! -f ~/.conan/settings.yml ]; then
