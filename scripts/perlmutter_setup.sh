f82f87f14cd1b2f959ed725bcabd36cf0ef361a1
Marco Minutoli <marco.minutoli@pnnl.gov>
2023-03-21 20:21:42 -0700

    Fix MPI related issues on Perlmutter.

--- scripts/perlmutter_setup.sh
+++ scripts/perlmutter_setup.sh
@@ -3,16 +3,18 @@
 echo 'Running the first time setup script'
 
 pip install --user "conan==1.59"
-pip install --user torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
-pip install --user torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cpu.html
 
 if [ ! -f ~/.conan/settings.yml ]; then
     conan config init
 fi
 conan profile new default --detect &> /dev/null
 conan profile update settings.compiler.libcxx=libstdc++ default
-conan profile update env.CC=$(which gcc) default
-conan profile update env.CXX=$(which g++) default
+# For unknown reasons conan detects the wrong version from
+# the Cray Compiler wrapper version string.
+conan profile update settings.compiler.version=11.2 default
+# Cray Machine have compiler wrappers providing MPI
+conan profile update env.CC=cc default
+conan profile update env.CXX=CC default
 
 if grep riscv $HOME/.conan/settings.yml; then
     echo RISCV support already added. Skipping.
