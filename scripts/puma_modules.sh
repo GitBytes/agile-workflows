9cfeca7810f2aa91c1dc57ed23cd0367cc29334a
Marco Minutoli <marco.minutoli@pnnl.gov>
2023-05-05 15:31:33 -0700

    Update Build scripts for puma with new compiler

--- scripts/puma_modules.sh
+++ scripts/puma_modules.sh
@@ -3,8 +3,8 @@
 module purge
 module load cmake/3.18.2
 module load git/2.17.0
-module load gcc/8.2.0
-module load openmpi/3.1.3
+module load gcc/11.2.0
+module load openmpi/4.1.5
 
 module load python &> /dev/null
 source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh
