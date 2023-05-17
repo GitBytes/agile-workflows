dc94d8289d8803896a06ef4c53d87006da61fcf2
John Feo <john.feo@pnnl.gov>
2023-05-17 12:31:42 -0700

    Fixing bug into the setup scripts

--- scripts/puma_setup.sh
+++ scripts/puma_setup.sh
@@ -9,7 +9,7 @@ if [ ! -f ~/.conan/settings.yml ]; then
     conan config init
 fi
 conan profile new default --detect &> /dev/null
-conan profile update settings.compiler.libcxx=libstdc++11 default
+conan profile update settings.compiler.libcxx=libstdc++ default
 
 if grep riscv $HOME/.conan/settings.yml; then
     echo RISCV support already added. Skipping.
