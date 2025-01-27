150764700e0e1c19ab899166837eb222e7595761
Marco Minutoli <marco.minutoli@pnnl.gov>
2025-01-27 09:36:02 -0800

    Remove not needed environment variables.

--- scripts/puma_modules.sh
+++ scripts/puma_modules.sh
@@ -11,18 +11,18 @@ source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh
 
 export PATH=$PATH:$HOME/.local/bin
 
-HWLOCROOT=$(pwd)/hwloc/install
-echo $HWLOCROOT
-
-if [ ! -d "hwloc" ]; then
-    git clone https://github.com/open-mpi/hwloc.git
-    cd hwloc
-    git checkout c80630218a1c11e1ccc3fa89155647c737385f59 # version 2.10
-    ./autogen.sh
-    ./configure --enable-plugins -prefix=$HWLOCROOT
-    make -j && make install
-fi
-
-
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HWLOCROOT/lib/pkgconfig
+# HWLOCROOT=$(pwd)/hwloc/install
+# echo $HWLOCROOT
+#
+# if [ ! -d "hwloc" ]; then
+#     git clone https://github.com/open-mpi/hwloc.git
+#     cd hwloc
+#     git checkout c80630218a1c11e1ccc3fa89155647c737385f59 # version 2.10
+#     ./autogen.sh
+#     ./configure --enable-plugins -prefix=$HWLOCROOT
+#     make -j && make install
+# fi
+#
+#
+# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HWLOCROOT/lib/pkgconfig
 
