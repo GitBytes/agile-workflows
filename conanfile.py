27991852687f6f0d409f290bc067bbada8a25d44
Marco Minutoli <marco.minutoli@pnnl.gov>
2025-05-02 16:05:59 -0700

    Add BSD3 license headers.

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,37 @@
+# The AGILE Workflows
+#
+# Copyright (c) 2025 Battelle Memorial Institute
+#
+# Battelle Memorial Institute (hereinafter Battelle) hereby grants permission to
+# any person or entity lawfully obtaining a copy of this software and associated
+# documentation files (hereinafter “the Software”) to redistribute and use the
+# Software in source and binary forms, with or without modification.  Such person
+# or entity may use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and may permit others to do so, subject to the
+# following conditions:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimers.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Other than as used herein, neither the name Battelle Memorial Institute or
+#    Battelle may be used in any form whatsoever without the express written
+#    consent of Battelle.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL BATTELLE OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from conans import ConanFile, CMake, tools
 
 
@@ -6,7 +40,6 @@ class AgileWorkflowsConan(ConanFile):
     version = "0.0.1"
     license = "APACHE 2.0"
     author = "<Put your name here> <And your email here>"
-    # url = "<Package recipe repository url here, for issues about the package>"
     description = "The AGILE Workflows"
     # topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
--- dataGenerators/reduceData.py
+++ dataGenerators/reduceData.py
@@ -1,3 +1,37 @@
+# The AGILE Workflows
+#
+# Copyright (c) 2025 Battelle Memorial Institute
+#
+# Battelle Memorial Institute (hereinafter Battelle) hereby grants permission to
+# any person or entity lawfully obtaining a copy of this software and associated
+# documentation files (hereinafter “the Software”) to redistribute and use the
+# Software in source and binary forms, with or without modification.  Such person
+# or entity may use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and may permit others to do so, subject to the
+# following conditions:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimers.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Other than as used herein, neither the name Battelle Memorial Institute or
+#    Battelle may be used in any form whatsoever without the express written
+#    consent of Battelle.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL BATTELLE OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import sys
 
@@ -11,8 +45,6 @@ line3 = "#types:STRING,UINT,UINT,UINT,UINT,UINT,UINT,USDATE,DOUBLE,DOUBLE\n"
 dataOut.write(line1)
 dataOut.write(line2)
 dataOut.write(line3)
-
-# include sale records lines to insure pattern
 lines = { "Sale,1419850416906085161,1128501731262832684,,,,2869238,09/28/2018,,\n",
           "Sale,477384404927196020,1128501731262832684,,,,271997,09/30/2018,,\n",
           "Sale,1472154222902711100,1128501731262832684,,,,185785,09/28/2018,,\n",
